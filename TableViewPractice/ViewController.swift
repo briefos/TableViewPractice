f112c13e5e7af5b885e568ade607d6397475110f
BriefOS <briefos@taeshawits-macbook-pro-2.local>
2017-02-03 18:27:44 +0700

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by BriefOS on 2/3/2560 BE.
+//  Copyright © 2560 BriefOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
